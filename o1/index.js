6f13761dcfabaf9a2ec4f0445ec1a7535d279f9a
99Gran <nini.g94@gmail.com>
2020-03-17 13:23:49 +0100

    Second commit

---
+++ o1/index.js
@@ -0,0 +1,8 @@
+let filmer = ["Spirited Away", "Up", "The Bucket List", "Bambi", "Titanic", "Churchill"];
+
+filmer = [...filmer, "Anna Karanina"]
+filmer = ["Mulan", ...filmer]
+
+filmer[2] = "Leon";
+
+console.log(filmer)
