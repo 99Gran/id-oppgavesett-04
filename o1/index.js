44e3e701453b2b5beb8c140e9ca1b252b9a4ab28
99Gran <nini.g94@gmail.com>
2020-03-17 13:50:00 +0100

    Third commit

--- o1/index.js
+++ o1/index.js
@@ -1,8 +1,38 @@
-let filmer = ["Spirited Away", "Up", "The Bucket List", "Bambi", "Titanic", "Churchill"];
+/* let filmer = ["Spirited Away", "Up", "The Bucket List", "Bambi", "Titanic", "Churchill"];
 
 filmer = [...filmer, "Anna Karanina"]
 filmer = ["Mulan", ...filmer]
 
 filmer[2] = "Leon";
 
-console.log(filmer)
+console.log(filmer)*/
+
+const listFilm = document.querySelector("#listFilm");
+const inpNyFilm = document.querySelector("#inpNyFilm");
+const btn = document.querySelector("#btn");
+
+let film = [];
+
+
+function visFilmer() {
+
+    listFilm.innerHTML = "";
+
+    for(const film of filmer) {
+        listFilm.innerHTML += `<li>${film}</li>`;
+    }
+    
+    inpNyFilm.value = "";
+    inpNyFilm.focus();
+    
+}
+
+
+
+
+function leggTilFilm() {
+    film = [inpNyFilm.value, ...film];
+    visFilmer();
+}
+
+btn.onclick = leggTilFilm;
